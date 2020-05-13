a9ab0bf59c6e50b542ce2d90de97a53383e50385
Ben Cohen <benjcohen1985@gmail.com>
2020-05-13 18:37:51 +0300

    Update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+// import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LandingPageComponent,
+    // FormsModule
   ],
   imports: [
+    MatMenuModule,
     BrowserModule,
-    AppRoutingModule
-  ],
+    MatSidenavModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
