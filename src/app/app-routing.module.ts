d4caf36d06739308cf1d43dde3d1195a1100651c
Ben Cohen <benjcohen1985@gmail.com>
2020-05-14 16:22:27 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddReviewComponent } from './add-review/add-review.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LandingPageComponent
+  },
+  {
+    path: 'post', component: AddReviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
