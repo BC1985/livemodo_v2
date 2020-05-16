9aaa34017c3cf107bb528621d70e1f31336fd7ed
Ben Cohen <benjcohen1985@gmail.com>
2020-05-16 14:59:37 +0300

    Destructure imports array Import form modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-// import { FormsModule } from '@angular/forms';
+import { AddReviewComponent } from './add-review/add-review.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { TextareaComponent } from './components/textarea/textarea.component';
 
+const modules = [
+  FormsModule,
+  ReactiveFormsModule,
+  MatMenuModule,
+  MatCardModule,
+  BrowserModule,
+  MatSidenavModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatButtonModule,
+  MatNativeDateModule,
+  MatInputModule,
+  MatRadioModule,
+  MatIconModule,
+  MatListModule,
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     LandingPageComponent,
-    // FormsModule
+    AddReviewComponent,
+    DatepickerComponent,
+    TextareaComponent,
   ],
-  imports: [
-    MatMenuModule,
-    BrowserModule,
-    MatSidenavModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatIconModule,
-    MatListModule
-   ],
   providers: [],
+  imports: [[...modules]],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
