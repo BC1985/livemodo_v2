c16bde1222a5bd54dcf454f29592b59f01f73d6a
Ben Cohen <benjcohen1985@gmail.com>
2020-05-27 11:29:53 +0300

    Add routing to browse component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddReviewComponent } from './add-review/add-review.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { BrowseReviewsComponent } from './browse-reviews/browse-reviews.component';
 
 const routes: Routes = [
   {
-    path: '', component: LandingPageComponent
+    path: '',
+    component: LandingPageComponent,
   },
   {
-    path: 'post', component: AddReviewComponent
-  }
+    path: 'post',
+    component: AddReviewComponent,
+  },
+  {
+    path: 'browse',
+    component: BrowseReviewsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
