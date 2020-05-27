c2ce9fcc9f4cf8a8199f1f6d44def03f497545c6
Ben Cohen <benjcohen1985@gmail.com>
2020-05-27 11:29:27 +0300

    Implement browse component Connect http module and connect to service

---
+++ src/app/browse-reviews/browse-reviews.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { PostService } from '../../services/post.service';
+@Component({
+  selector: 'app-browse-reviews',
+  templateUrl: './browse-reviews.component.html',
+  styleUrls: ['./browse-reviews.component.scss'],
+})
+export class BrowseReviewsComponent implements OnInit {
+  public posts = [];
+  constructor(private _postService: PostService) {}
+
+  ngOnInit() {
+    this._postService.getPosts().subscribe((data) => (this.posts = data));
+  }
+}
