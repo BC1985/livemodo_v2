b86dff5b083031079996e3e26c50e26ba1b8c41c
Ben Cohen <benjcohen1985@gmail.com>
2020-06-16 19:19:41 +0300

    Retrieve external component

--- src/app/components/textarea/textarea.component.ts
+++ src/app/components/textarea/textarea.component.ts
@@ -2,16 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-textarea',
-  template: `
-    <textarea
-      matInput
-      cdkTextareaAutosize
-      #autosize="cdkTextareaAutosize"
-      cdkAutosizeMinRows="1"
-      cdkAutosizeMaxRows="5"
-    >
-    </textarea>
-  `,
+  templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
 })
 export class TextareaComponent implements OnInit {
