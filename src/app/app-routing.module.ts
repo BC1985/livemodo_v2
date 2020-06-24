05abb7a1abe3c9ddc6c2aa0b46aefa1d2e46fa0c
Ben Cohen <benjcohen1985@gmail.com>
2020-06-24 09:58:03 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddReviewComponent } from './add-review/add-review.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowseReviewsComponent } from './browse-reviews/browse-reviews.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
     path: 'browse',
     component: BrowseReviewsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterUserComponent,
   },
 ];
 
