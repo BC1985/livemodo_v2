7daae893e256ca566202096e3b170d68a9d06a97
Ben Cohen <benjcohen1985@gmail.com>
2020-06-24 09:58:44 +0300

    Create review model

---
+++ src/app/Models/Review.ts
@@ -0,0 +1,10 @@
+export interface Review {
+  id: Number;
+  performer: string;
+  content: string;
+  showDate: string;
+  tagline: string;
+  username: string;
+  rating: Int16Array;
+  venue: string;
+}
