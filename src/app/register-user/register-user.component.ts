92cd2ee31e10599b4b42d6a115909cf4cba9debd
Ben Cohen <benjcohen1985@gmail.com>
2020-06-24 09:59:30 +0300

    Create user registration form

---
+++ src/app/register-user/register-user.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { UserData } from '../user-data';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-register-user',
+  templateUrl: './register-user.component.html',
+  styleUrls: ['./register-user.component.scss'],
+})
+export class RegisterUserComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
+          ),
+        ],
+      ],
+      repeatPassword: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'
+          ),
+        ],
+      ],
+    });
+  }
+  get input() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+  }
+  log(x, y) {
+    console.log(x, y);
+  }
+}
