a770da03f89a33d96219fd27558bf3c476f28a62
Ben Cohen <benjcohen1985@gmail.com>
2020-06-24 10:00:27 +0300

    Change review object properties

--- src/app/add-review/add-review.component.ts
+++ src/app/add-review/add-review.component.ts
@@ -3,16 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-review',
   templateUrl: './add-review.component.html',
-  styleUrls: ['./add-review.component.scss']
+  styleUrls: ['./add-review.component.scss'],
 })
 export class AddReviewComponent implements OnInit {
 
   constructor() { }
 
-  public band_name: string;
-  public show_date: number;
-  public venue: string;
-  public review: string;
+  public Performer: string;
+  public Username: string;
+  public ShowDate: string;
+  public Venue: string;
+  public Content: string;
+  public Tagline: string;
+  public rating: Int16Array;
 
   onSubmit(){}
   log(x) {
