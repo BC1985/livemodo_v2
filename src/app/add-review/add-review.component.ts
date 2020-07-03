73f28550ae75050d7f4d6c7cfd4d06c4f1e26f9b
Ben Cohen <benjcohen1985@gmail.com>
2020-07-03 14:22:36 +0300

    Change form type to reactive Import Moment modules Implement post method to API

--- src/app/add-review/add-review.component.ts
+++ src/app/add-review/add-review.component.ts
@@ -1,25 +1,87 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from '../Models/Review';
+import * as _moment from 'moment';
+import { ReviewService } from '../../services/review.service';
+import { FormGroup, FormBuilder, Validators,} from '@angular/forms';
 
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+  },
+  display: {
+    dateInput: 'L',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 @Component({
   selector: 'app-add-review',
   templateUrl: './add-review.component.html',
   styleUrls: ['./add-review.component.scss'],
+  providers: [{
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class AddReviewComponent implements OnInit {
+  addReviewForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private _reviewService: ReviewService
+  ) {}
 
-  public Performer: string;
-  public Username: string;
-  public ShowDate: string;
-  public Venue: string;
-  public Content: string;
-  public Tagline: string;
-  public rating: Int16Array;
+  ngOnInit() {
+    this.addReviewForm = this.formBuilder.group({
+      performer: ['', Validators.required],
+      showDate: [_moment(), Validators.required],
+      venue: ['', Validators.required],
+      content: ['', Validators.required],
+      rating: ['', Validators.required],
+      tagline:['']
+    });
+  }
+  postReview() {
+    const {
+      performer,
+      tagline,
+      content,
+      showDate,
+      venue,
+      rating,
+    } = this.addReviewForm.controls;
 
-  onSubmit(){}
+    var review = new Review();
+    
+    review.Performer = performer.value;
+    review.ShowDate = _moment(showDate.value._d).format('MMMM Do, YYYY');
+    review.Venue = venue.value;
+    review.Content = content.value;
+    review.Rating = rating.value;
+    review.Tagline = tagline.value;
+    console.log(review.ShowDate, review.Venue, review.Performer, review.Content, review.Tagline, review.Rating);
+    
+    this._reviewService.postReview(review).subscribe((data) => {
+      review = data;
+    });
+  }
+  get input() {
+    return this.addReviewForm.controls;
+  }
+
   log(x) {
     console.log(x);
   }
-  ngOnInit(): void {}
+  onSubmit() {
+    this.postReview()
+    this.submitted = true;
+  }
 }
