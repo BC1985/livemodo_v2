892d8341affe82d6341ef034a622512934f25194
Ben Cohen <benjcohen1985@gmail.com>
2020-07-03 14:25:09 +0300

    Create sarch component

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { SpotifyService } from "../../../services/spotify.service";
-import { Artist } from "../../Models/Artists";
+import { Component, OnInit, Input } from '@angular/core';
+import { SpotifyService } from '../../../services/spotify.service';
+import { Artist } from '../../Models/Artists';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-searchStr: string;
-searchResults: Artist[]
-  constructor(private _spotifyService: SpotifyService) { }
-  getArtist(){
+  searchStr: string;
+  searchResults: Artist[];
+  constructor(private _spotifyService: SpotifyService) {}
+  getArtist() {
     this._spotifyService.searchMusic(this.searchStr).subscribe((data) => {
-      this.searchResults= data.artists.items;
-      
-      console.log('Artist-',data.artists.items[0].name, 'Genres-',data.artists.items[0].genres)
+      this.searchResults = data.artists.items;
+
+      console.log(
+        'Artist-',
+        data.artists.items[0].name,
+        'Genres-',
+        data.artists.items[0].genres
+      );
     });
   }
-getAuth(){
-  this._spotifyService.getAccessToken()
-}
-  ngOnInit(): void {
-   
+  getAuth() {
+    this._spotifyService.getAccess();
   }
-
+  ngOnInit(): void {}
 }
